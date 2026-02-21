c957293832ed8ba67de8a6b93f1908a357278ef7
naveen123bh <149930156+naveen123bh@users.noreply.github.com>
2026-02-21 15:31:12 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -7,7 +7,7 @@ import difflib
 # KDHA NOTE
 # ==================================
 st.markdown("<h3 style='color:purple;'>KDHA</h3>", unsafe_allow_html=True)
-st.markdown("<i>Note: This app is under development and consideration</i>", unsafe_allow_html=True)
+st.markdown("<i>Note: This app is under development and consideration.</i>", unsafe_allow_html=True)
 
 # ==================================
 # CSSD TECHNICIAN MASTER LIST
