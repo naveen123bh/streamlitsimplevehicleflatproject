f3c22e40d8ad2bd3f11957f31bffd13c511c904f
naveen123bh <149930156+naveen123bh@users.noreply.github.com>
2026-02-23 12:54:05 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -10,7 +10,7 @@ import pytz
 from quotes import get_random_quote  # <- import quotes
 
 # ==============================
-# 
+
 # ==============================
 hospital_image_url = "https://i.ibb.co/7NYqvcHz/hospital.jpg"
 st.image(hospital_image_url, width=400)
