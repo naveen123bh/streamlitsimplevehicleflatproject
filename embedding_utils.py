ad8be53b7cd68f38575d9f6035499b7df83020f1
naveen123bh <149930156+naveen123bh@users.noreply.github.com>
2026-02-25 12:40:28 +0530

    Update embedding_utils.py

--- embedding_utils.py
+++ embedding_utils.py
@@ -1,21 +1,15 @@
 import torch
 from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
-import numpy as np
 
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 def get_embedding(image_file):
     image = Image.open(image_file).convert("RGB")
-
     inputs = processor(images=image, return_tensors="pt")
 
     with torch.no_grad():
         image_features = model.get_image_features(**inputs)
-
-    image_features = image_features[0]  # batch dimension हटाया
-
-    image_features = image_features / image_features.norm()
 
     return image_features.detach().cpu().numpy().flatten()
